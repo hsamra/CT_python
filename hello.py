b8c6663c30fc0ca3eda1e24bf072b7310cbb8a11
Samra Husremović <samra.husremovic@gmail.com>
2011-10-22 13:54:48 +0200

    verzija 2
    if-else konstrukt u python-u koji rješava isti zadatak kao što je
    navedeno u članku
    http://hernad.bring.out.ba/hello-programiranje-al-cestito

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print "hello world"
+samrin_babo = "hernad"
+# samrin_babo = "huso"
+
+if samrin_babo == "hernad":
+    print "hello from Samra Husremovic"
+else:
+    print "hello from neka druga Samra"
+
